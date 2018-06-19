c683942832e33b00027b4127b32bb15eb899b1b8
Admin <admin@mac-admin.local>
2018-06-19 14:06:01 +0300

    Initial Commit

---
+++ UIViewGeometryTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIViewGeometryTest
+//
+//  Created by Admin on 19.06.2018.
+//  Copyright © 2018 Sergio Lechini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIViewGeometryTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIViewGeometryTest
+//
+//  Created by Admin on 19.06.2018.
+//  Copyright © 2018 Sergio Lechini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
